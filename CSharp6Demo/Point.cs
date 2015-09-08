88823dd07f71a519ee299998bd1c7740088e5fcb
RyanSpears <sperj001@hotmail.com>
2015-09-08 21:59:22 +0930

    Initial commit

---
+++ CSharp6Demo/Point.cs
@@ -0,0 +1,54 @@
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace CSharp6Demo
+{
+    public class PointCSharp5
+    {
+        private readonly int _x;
+        private readonly int _y;
+
+        public int X { get { return _x; } } // immutable getter
+        public int Y { get { return _y; } } // immutable getter
+
+        public PointCSharp5(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0},{1})", X, Y); // String formatting
+        }
+
+        public double Distance
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+        public static PointCSharp5 operator +(PointCSharp5 p1, PointCSharp5 p2)
+        {
+            return new PointCSharp5(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public static PointCSharp5 operator -(PointCSharp5 p1, PointCSharp5 p2)
+        {
+            return new PointCSharp5(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public static PointCSharp5 FromJson(JObject json)
+        {
+            if (json == null ||
+                json["x"] == null ||
+                json["x"].Type != JTokenType.Integer ||
+                json["y"] == null ||
+                json["y"].Type != JTokenType.Integer)
+            {
+                throw new ArgumentException("The paramater is not shaped like a point", "json");
+            }
+
+            return new PointCSharp5((int)json["x"], (int)json["y"]);
+        }
+    }
+}
---
+++ CSharp6Demo/PointCSharp6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace CSharp6Demo
+{
+    public class Point
+    {
+        public int X { get; } // getter-only auto-property
+        public int Y { get; } // getter-only auto-property
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => $"({X},{Y})"; // string interpolation
+
+        public double Distance => Math.Sqrt(X* X + Y* Y); // expression body on property
+
+        public static Point operator +(Point p1, Point p2) => new Point(p1.X + p2.X, p1.Y + p2.Y); // expression body on method
+
+        public static Point operator -(Point p1, Point p2) => new Point(p1.X - p2.X, p1.Y - p2.Y); // expression body on method
+
+        public static Point FromJson(JObject json)
+        {
+            if (json == null ||
+                json["x"]?.Type != JTokenType.Integer || // null conditional operator
+                json["y"]?.Type != JTokenType.Integer)
+            {
+                throw new ArgumentException("The paramater is not shaped like a point", nameof(json)); // nameof expression
+            }
+
+            return new Point((int)json["x"], (int)json["y"]);
+        }
+    }
+}
